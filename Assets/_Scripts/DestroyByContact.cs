316ed3206cceff9bd1bbbecc79e8474115a0410b
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-01-10 22:07:45 -0700

    Finished Asteroid Prefab and explosions

--- Assets/_Scripts/DestroyByContact.cs
+++ Assets/_Scripts/DestroyByContact.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider other) {
         if (other.tag != "Boundary") {
+            Instantiate(explosion, transform.position, transform.rotation);
+            if (other.tag == "Player") {
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            }
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
