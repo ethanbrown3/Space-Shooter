dd6d2be7b96e800c010c1f6e155032b1eeb8ab9a
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-02-07 23:16:24 -0700

    Added wave pattern to Evasive Maneuver

--- Assets/_Scripts/EvasiveManeuver.cs
+++ Assets/_Scripts/EvasiveManeuver.cs
@@ -7,11 +7,13 @@ public class EvasiveManeuver : MonoBehaviour {
     public float tilt;
     public float smoothing;
     public float dodge;
+
     public Vector2 startWait;
     public Vector2 maneuverTime;
     public Vector2 maneuverWiat;
     public Boundary boundary;
 
+	private float wave;
     private float currentSpeed;
     private float targetManeuver;
     private Rigidbody rb;
@@ -19,23 +21,42 @@ public class EvasiveManeuver : MonoBehaviour {
 	void Start () {
         rb = GetComponent<Rigidbody>();
         currentSpeed = rb.velocity.z;
-        StartCoroutine(Evade());
+		if (Random.value < 0.5f) {
+			StartCoroutine (Evade ());
+		} else {
+			StartCoroutine (Wave ());
+		}
 	}
 	
     IEnumerator Evade () {
         yield return new WaitForSeconds (Random.Range(startWait.x, startWait.y));
 
         while (true) {
-            targetManeuver = Random.Range (1, dodge) * -Mathf.Sign (transform.position.x); // MathF.Sign will keep the ships from dodging off the screen
+            targetManeuver = Random.Range (1, dodge) * -Mathf.Sign (transform.position.x); 
+			Debug.Log (targetManeuver);
             yield return new WaitForSeconds (Random.Range (maneuverTime.x, maneuverTime.y));
             targetManeuver = 0;
             yield return new WaitForSeconds(Random.Range (maneuverTime.x, maneuverTime.y));
         }
     }
 
+	void Update () {
+		wave = Mathf.Sin(Time.time * 3) * dodge;
+	}
+
+	IEnumerator Wave () {
+ 		float startDirection = Mathf.Sign (transform.position.x);
+		while (true) {
+			targetManeuver = wave * startDirection;
+			Debug.Log (targetManeuver);
+			yield return new WaitForSeconds(.001f);
+		}
+	}
+
 	void FixedUpdate () {
+		Debug.Log (targetManeuver);
         float newManeuver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
-        rb.velocity = new Vector3 (newManeuver, 0.0f, currentSpeed);
+		rb.velocity = new Vector3 (newManeuver, 0.0f, currentSpeed);
         rb.position = new Vector3(
             Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
             0.0f,
@@ -44,4 +65,10 @@ public class EvasiveManeuver : MonoBehaviour {
 
         rb.rotation = Quaternion.Euler(0.0f, 0.0f, (rb.velocity.x * -tilt));
     }
+
+	Vector3 DiagonalMove () {
+	
+		return new Vector3 (-2.0f, 0.0f, currentSpeed);
+
+	}
 }
