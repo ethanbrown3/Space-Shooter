db8ffff32db2fdce9074b77f99ee7ab0286c2c5e
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-02-08 23:48:41 -0700

    Added code for charged shot and charged shot prefab

--- Assets/_Scripts/Gun.cs
+++ Assets/_Scripts/Gun.cs
@@ -5,15 +5,21 @@ public class Gun : MonoBehaviour
 {
 
 	public GameObject shot;
+	public GameObject chargeShot;
 	public Transform shotSpawn;
 	public Transform shotSpawn2;
 	public Transform shotSpawn3;
 	public float fireRate;
 	public AudioSource weaponAudio;
 	public float upgradeTime = 10;
+	public float maxCharge;
+	public float chargeRate;
+	public float chargeDamage;
+	public float startCharge;
 
 	public bool isRapid = false;
 	public bool isSpread = false;
+	public bool canCharge = false;
 
 	private float nextFire = 0.0f; // make sure you can fire again until time has passed this value
 	private float timeRapid = 0.0f;
@@ -25,8 +31,10 @@ public class Gun : MonoBehaviour
 
 	void Update() {
 		// shots fired
-		if (Input.GetButton("Fire1") && Time.time > nextFire) {
-			fireGun ();
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+				fireGun ();
+		} else if (canCharge ) {
+			chargeUp ();
 		}
 		if (Time.time > timeSpread) {
 			isSpread = false;
@@ -58,10 +66,28 @@ public class Gun : MonoBehaviour
 		}
 	}
 
+	void chargeUp() {
+		if (Input.GetMouseButtonDown(1)) {
+			startCharge = Time.time;
+		}
+		if (Input.GetMouseButtonUp (1)) {
+			chargeDamage = (Time.time - startCharge) * chargeRate;
+			if (chargeDamage > maxCharge) {
+				chargeDamage = maxCharge;
+			}
+			createBolt ();
+			GameObject bolt = Instantiate(chargeShot, shotSpawn.position, shotSpawn.rotation);
+			bolt.transform.localScale = new Vector3(chargeDamage/2.0f, chargeDamage/2.0f, chargeDamage/2.0f);
+			shot.transform.position = shotSpawn.position;
+			weaponAudio.Play();
+		}		
+	}
+
+
 	private Rigidbody createBolt() {
 		GameObject bolt = Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-		shot.transform.position = shotSpawn.position;
-		return shot.GetComponent<Rigidbody>();
+		bolt.transform.position = shotSpawn.position;
+		return bolt.GetComponent<Rigidbody>();
 	}
 
 }
